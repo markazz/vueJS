6068abc65106f471b6effe4267e980f76715c97d
Mark Rasheed <azizconqueror1@gmail.com>
2018-07-22 20:48:20 +0100

    Update gridComponent.js

--- gridComponent.js
+++ gridComponent.js
@@ -1,4 +1,5 @@
 
+
 // register the grid component 
 
 Vue.component('demo-grid',{
@@ -8,5 +9,37 @@ template:'#grid-template',
 props:{data :Array,columns:Array,filterKey:String},
 
 
+data: function(){
 
+// create an empty array
+var sortOrders = {}
+
+
+// loop through the columns
+this.columns.forEach(function(key){
+
+sortOrders[key]=1
 })
+
+
+return{
+
+sortKey:'',
+sortOrders:sortOrders
+}
+
+
+},
+
+
+
+
+
+// end of vueComponent
+})
+
+
+
+
+
+
